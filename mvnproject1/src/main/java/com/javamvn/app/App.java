7c855b5c309c09b79c632da83a83957e3878199e
Rajivv Rajivv <xrajivv@nymfe58.fi.muni.cz>
2018-09-24 11:29:51 +0200

    first maven

---
+++ mvnproject1/src/main/java/com/javamvn/app/App.java
@@ -0,0 +1,13 @@
+package com.javamvn.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
